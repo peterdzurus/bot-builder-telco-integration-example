fe9b48a819df6d2ca4230fa38e574ccb198d6d26
Peter Dzuruš <dzurus.peter@gmail.com>
2019-04-25 11:09:51 +0200

    remove cors

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(cors());
-
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
